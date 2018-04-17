c62e8afc8b6a8f3934fd45036d9953c0fb174350
Dwade <hedewei@shmahine.com>
2018-04-17 23:27:48 +0800

    gateway

---
+++ main/uart1_events.h
@@ -0,0 +1,8 @@
+#ifndef _UART1_EVENTS_H_
+#define _UART1_EVENTS_H_
+
+
+void uart1_events_task_create();
+
+
+#endif /* end define */
---
+++ main/usart1.h
@@ -0,0 +1,6 @@
+#ifndef _USART1_H_
+#define _USART1_H_
+
+void uart1_task_Create(void);
+
+#endif /* end define*/
